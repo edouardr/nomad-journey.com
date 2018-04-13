3bb827ce8f619bca4b1f799078a2f8b34f0aeb5f
edouardr <riviere.edouard@hotmail.fr>
2018-04-13 22:25:02 +1000

    Cleaning styles for article tiles

--- components/article-tile.vue
+++ components/article-tile.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="summary summary--article"
+  <div class="card"
     @mouseover="hovered = !hovered"
     @mouseleave="hovered = !hovered">
-    <router-link :to="url" @onclick="select(id)" class="summary__block-link">{{title}}</router-link>
-    <div class="summary__img">
-      <div :class="'ri' + (hovered ? ' hovered': '')">
+    <router-link :to="url" @onclick="select(id)" class="card-link">{{title}}</router-link>
+    <div class="card-image ri">
+      <figure :class="'image is-5by3'+ (hovered ? ' hovered': '')">
         <img :src="imgUrl" :alt="imgAlt">
-      </div>
+      </figure>
     </div>
-    <div class="summary__text">
-        <h3 class="summary__title">
+    <div class="card-content">
+        <p class="title is-5">
           <router-link :to="url">{{title}}</router-link>
-        </h3>
-        <p>
+        </p>
+        <p class="subtitle is-7">
           {{text}}
         </p>
     </div>
@@ -39,23 +39,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .summary--article .summary__img {
-      padding-top: 66.66%;
-  }
-  @media print, screen and (min-width: 32em){
-    .summary-grid .summary {
-      margin: 20px 10px 0;
-    }
-  }
-  .summary-grid--articles .summary, .summary-grid--recipes .summary {
-    width: calc(100% - 10px);
-  }
-
-  .summary-grid .summary {
-    width: calc(50% - 10px);
-    margin: 20px 5px 0;
-  }
-  .summary {
+  .card {
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    box-shadow: none;
     width: 100%;
     position: relative;
     text-align: left;
@@ -63,9 +50,10 @@ export default {
     transition: background-color .5s;
     overflow: hidden;
     cursor: pointer;
+    color: $water;
   }
 
-  .summary__block-link {
+  .card-link {
     display: block;
     position: absolute;
     top: 0;
@@ -79,9 +67,8 @@ export default {
     z-index: 3;
   }
 
-  .summary__img {
+  .card-image {
     position: relative;
-    padding-top: 100%;
     overflow: hidden;
     z-index: 1;
 
@@ -92,56 +79,32 @@ export default {
       position: absolute;
       top: 0;
       left: 0;
-      -webkit-transition: all 1s ease; /* Safari and Chrome */
-      -moz-transition: all 1s ease; /* Firefox */
-      -ms-transition: all 1s ease; /* IE 9 */
-      -o-transition: all 1s ease; /* Opera */
+    }
+
+    .image {
+      -webkit-transition: all 1s ease;
+      -moz-transition: all 1s ease;
+      -ms-transition: all 1s ease;
+      -o-transition: all 1s ease;
       transition: all 1s ease;
 
       &.hovered {
-        -webkit-transform:scale(1.25); /* Safari and Chrome */
-        -moz-transform:scale(1.25); /* Firefox */
-        -ms-transform:scale(1.25); /* IE 9 */
-        -o-transform:scale(1.25); /* Opera */
+        -webkit-transform:scale(1.25);
+        -moz-transform:scale(1.25);
+        -ms-transform:scale(1.25);
+        -o-transform:scale(1.25);
         transform:scale(1.25);
       }
-
-      img {
-        max-width: 100%;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        -o-object-fit: cover;
-        object-fit: cover;
-        font-family: "object-fit: cover;";
-        vertical-align: top;
-      }
     }
   }
 
-  @media print, screen and (min-width: 64em) {
-    .summary--article .summary__text {
-      font-size: 1rem;
-      line-height: 1.375;
-    }
-  }
-  @media print, screen and (min-width: 48em) {
-    .summary--article .summary__text, .summary--product .summary__text {
-      border-width: 0.09em;
-    }
-  }
-
-  .summary--article .summary__text {
+  .card .card-content {
     border: 2px solid $water;
     background: transparent;
-    font-size: .875rem;
-    line-height: 1.43;
     padding-top: calc(66% + 5px);
     margin-top: calc(-66% + 3px);
   }
-  .summary__text {
+  .card-content {
     position: relative;
     background-color: #fff;
     padding: 10px;
@@ -150,35 +113,4 @@ export default {
     z-index: 2;
     pointer-events: none;
   }
-
-  @media screen and (min-width: 48em) {
-    .summary-grid--articles .summary, .summary-grid--recipes .summary {
-      width: calc(33.333% - 20px);
-    }
-  }
-
-  .summary--article .summary__title {
-    font-size: 1.125rem;
-    margin-bottom: .5rem;
-  }
-  @media print, screen and (min-width: 60em){
-    .summary__title {
-      font-size: 1.125rem;
-    }
-  }
-  @media print, screen and (min-width: 48em){
-    .summary__title {
-      font-size: 1rem;
-    }
-  }
-  .summary__title {
-    position: relative;
-    font-size: 1rem;
-    margin-bottom: 0;
-  }
-
-  .summary--article .summary__title>a {
-    color: $water;
-  }
-
 </style>
