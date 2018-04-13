48e85b79c17d3d8496093f148aa4a42363d9e54c
edouardr <riviere.edouard@hotmail.fr>
2018-04-14 00:13:58 +1000

    Destination list item style

--- components/destination-group.vue
+++ components/destination-group.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="columns tile is-ancestor">
-    <div class="column tile is-vertical"
+  <div class="columns is-multiline">
+    <div class="column is-one-third"
           v-for="destination in destinations"
           :key="destination.id"
           img-top>
-      <div class="overlay">
-        <div class="banner-hover">
-          <router-link :to="`/${destination.url}`">
-            <h3 class="tile-content">{{destination.title}}</h3>
-          </router-link>
-        </div>
-        <div class="col-bg" :style="`background-image: url(${destination.img.url});background-size: cover;`"></div>
+        <div class="card">
+          <router-link :to="`/${destination.url}`"></router-link>
+          <div class="card-image">
+            <figure class="image is-16by9">
+              <img :src="destination.img.url" alt="imgAlt">
+            </figure>
+          </div>
+          <div class="card-content">
+            <p class="title is-1 is-spaced has-text-white">{{destination.title}}</p>
+          </div>
       </div>
     </div>
   </div>
@@ -23,46 +26,80 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .overlay .banner-hover {
-    opacity: 1;
-    background: rgba(0, 0, 0, 0.6);
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-  }
-  .overlay {
-    overflow: hidden;
+  .card {
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    width: 100%;
     position: relative;
-    background-color: rgba(60, 60, 60, .85);
-  }
-  .overlay .banner-hover a {
-    display: block;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
-  .tile-content {
-    color: #fff;
-    border-top: 1px solid #fff;
-    border-bottom: 1px solid #fff;
-    padding: 20px 0;
-    margin: 50px 40px;
-    text-align: center;
-    font-size: 16px;
-    letter-spacing: 2px;
+    text-align: left;
+    padding-bottom: 5px;
+    transition: background-color .5s;
+    overflow: hidden;
+    cursor: pointer;
+    color: $water;
 
-    &:hover {
-      color: $water;
-      border-color: $water;
+    .card-content {
+      border: 2px solid $water;
+      background: transparent;
     }
   }
-  .col-bg
-  {
-    min-height:155px;
+
+  .card-image {
+    display: block;
+    position: relative;
+    padding-top: 50%;
+    overflow: hidden;
+    z-index: 10;
+
+    .image {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      padding-top: 0!important;
+
+      img {
+        -o-object-fit: cover;
+        object-fit: cover;
+        font-family: "object-fit: cover;";
+      }
+
+      &:after {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: rgba(44,22,26,.45);
+        z-index: 2;
+      }
+    }
+  }
+
+  .card-content {
+    display: flex;
+    z-index: 20;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    min-height: calc(100% - 24px);
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    right: 10px;
+    border-width: 3px;
+    padding: 20px;
+    margin: 0;
   }
 </style>
