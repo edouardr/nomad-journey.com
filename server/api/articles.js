7055efee38daa8d7f20be7d40a0c8f7cc823de04
edouardr <riviere.edouard@hotmail.fr>
2018-04-20 23:58:42 +1000

    Create a Json helper to handle circular reference removal

--- server/api/articles.js
+++ server/api/articles.js
@@ -1,43 +1,19 @@
 import { Router } from 'express'
+import { JsonHelper } from '../helpers/json-helper'
 import { ArticleService } from '../services/article-service'
 
+const helper = new JsonHelper()
 const router = Router()
 const service = new ArticleService()
 
 router.get('/articles/:lang/:slug', async (req, res, next) => {
   const response = await service.get(req.params.lang, req.params.slug)
-  var cache = []
-  let restoredObject = JSON.stringify(response.firstItem, function (key, value) {
-    if (typeof value === 'object' && value !== null) {
-      if (cache.indexOf(value) !== -1) {
-        // Circular reference found, discard key
-        return
-      }
-      // Store value in our collection
-      cache.push(value)
-    }
-    return value
-  })
-  cache = null // Enable garbage collection
-  res.json(JSON.parse(restoredObject))
+  res.json(JSON.parse(helper.removeCircularReferences(response.firstItem)))
 })
 
 router.get('/articles/latest/:lang/:limit', async (req, res, next) => {
   const response = await service.getLatest(req.params.lang, req.params.limit)
-  var cache = []
-  let restoredObject = JSON.stringify(response.items, function (key, value) {
-    if (typeof value === 'object' && value !== null) {
-      if (cache.indexOf(value) !== -1) {
-        // Circular reference found, discard key
-        return
-      }
-      // Store value in our collection
-      cache.push(value)
-    }
-    return value
-  })
-  cache = null // Enable garbage collection
-  res.json(JSON.parse(restoredObject))
+  res.json(JSON.parse(helper.removeCircularReferences(response.items)))
 })
 
 export default router
