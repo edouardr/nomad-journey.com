3f3f86c2fe3e303dc6aaa2cdabb385dfabdc24ac
edouardr <riviere.edouard@hotmail.fr>
2018-04-20 23:58:42 +1000

    Create a Json helper to handle circular reference removal

--- server/api/itinerary.js
+++ server/api/itinerary.js
@@ -1,26 +1,15 @@
 import { Symbols } from '../../constants'
 import { Router } from 'express'
+import { JsonHelper } from '../helpers/json-helper'
 import { ItineraryService } from '../services/itinerary-service'
 
+const helper = new JsonHelper()
 const router = Router()
 const service = new ItineraryService()
 
 router.get('/itinerary/:lang', async (req, res, next) => {
   const response = await service.get(req.params.lang, Symbols.CODENAMES.ITINERARY)
-  var cache = []
-  let restoredObject = JSON.stringify(response.item, function (key, value) {
-    if (typeof value === 'object' && value !== null) {
-      if (cache.indexOf(value) !== -1) {
-        // Circular reference found, discard key
-        return
-      }
-      // Store value in our collection
-      cache.push(value)
-    }
-    return value
-  })
-  cache = null // Enable garbage collection
-  res.json(JSON.parse(restoredObject))
+  res.json(JSON.parse(helper.removeCircularReferences(response.item)))
 })
 
 export default router
--- server/api/landing-page.js
+++ server/api/landing-page.js
@@ -1,25 +1,14 @@
 import { Router } from 'express'
+import { JsonHelper } from '../helpers/json-helper'
 import { LandingPageService } from '../services/landing-page-service'
 
+const helper = new JsonHelper()
 const router = Router()
 const service = new LandingPageService()
 
 router.get('/landing-page/:lang/:codename', async (req, res, next) => {
   const response = await service.get(req.params.lang, req.params.codename)
-  var cache = []
-  let restoredObject = JSON.stringify(response.item, function (key, value) {
-    if (typeof value === 'object' && value !== null) {
-      if (cache.indexOf(value) !== -1) {
-        // Circular reference found, discard key
-        return
-      }
-      // Store value in our collection
-      cache.push(value)
-    }
-    return value
-  })
-  cache = null // Enable garbage collection
-  res.json(JSON.parse(restoredObject))
+  res.json(JSON.parse(helper.removeCircularReferences(response.item)))
 })
 
 export default router
