f65618a66d8f6f67fd3861e531a015d1f3fd5571
edouardr <riviere.edouard@hotmail.fr>
2018-04-21 20:35:17 +1000

    Organize grid to take care of portrait oriented image

--- components/image-gallery.vue
+++ components/image-gallery.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="gallery">
-    <div class="gallery-item"
-      v-lazy-container="{ selector: 'img' }"
-      v-for="image in orderImages(images.assets)"
-      :key="image.id">
-      <img class="gallery-image" :data-src="image.url" :alt="image.description">
+  <div class="grid-container">
+    <div class="grid">
+      <div class="item"
+        v-lazy-container="{ selector: 'img' }"
+        v-for="image in orderImages(images.assets)"
+        :key="image.id">
+        <img :data-src="image.url" :alt="image.description">
+
+      </div>
     </div>
   </div>
 </template>
@@ -27,66 +30,54 @@ export default {
 
 
 <style lang="scss" scoped>
-  .gallery {
-    display: flex;
-    flex-wrap: wrap;
-    margin: -1rem -1rem;
-  }
+  .grid-container {
+    display: none;
 
-  .gallery-item {
-    flex: 1 0 24rem;
-    margin: 1rem;
-    overflow: hidden;
-  }
-
-  .gallery-image {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    transition: transform 400ms ease-out;
-
-    &:hover {
-      transform: scale(1.15);
+    @supports(display: grid) {
+      display: block;
     }
   }
 
-  @supports (display: grid) {
-    .gallery {
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(32rem, 1fr));
-      grid-gap: 0.5rem;
-    }
-
-    .gallery,
-    .gallery-item {
-      margin: 0;
-    }
+  .grid {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(28rem, 1fr));
+    grid-auto-flow: row dense;
 
     @media (max-width: 575.98px) {
-      .gallery {
-        grid-template-columns: repeat(1, 1fr);
-      }
+      grid-template-columns: repeat(1, 1fr);
     }
     @media (max-width: 768.98px) and (min-width: 576px) {
-      .gallery {
-        grid-template-columns: repeat(1, 1fr);
-      }
+      grid-template-columns: repeat(1, 1fr);
     }
     @media (max-width: 991.98px) and (min-width: 768px) {
-      .gallery {
-        grid-template-columns: repeat(1, 1fr);
-      }
+      grid-template-columns: repeat(1, 1fr);
     }
     @media (max-width: 1199.98px) and (min-width: 992px) {
-      .gallery {
-        grid-template-columns: repeat(2, 1fr);
-      }
+      grid-template-columns: repeat(2, 1fr);
     }
     @media (min-width: 1200px) {
-      .gallery {
-        grid-template-columns: repeat(2, 1fr);
-      }
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  .item {
+    display: flex;
+    flex-direction: column;
+    justify-self: stretch;
+    box-sizing: border-box;
+    grid-row-end: span 2;
+    color: #fff;
+
+    &__details {
+      position: relative;
+      z-index: 1;
+      padding: 15px;
+      color: #444;
+      background: #fff;
+      text-transform: lowercase;
+      letter-spacing: 1px;
+      color: #828282;
     }
   }
 </style>
