fff0014155a44d96a1b0e59dae8d4aa43c5ec296
edouardr <riviere.edouard@hotmail.fr>
2018-04-21 23:20:20 +1000

    location field constant value

--- content-types.js
+++ content-types.js
@@ -3,6 +3,7 @@ export const ContentTypes = {
     codeName: 'article',
     fields: {
       bodyText: 'body_text',
+      location: 'location',
       images: 'images',
       posted: 'posted',
       urlSlug: 'slug'
