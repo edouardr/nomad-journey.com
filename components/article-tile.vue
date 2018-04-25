c585447893ded0e91e194d7d56933922e3c35b34
edouardr <riviere.edouard@hotmail.fr>
2018-04-25 11:55:08 +1000

    WIP: Vuejs Transition

--- components/article-tile.vue
+++ components/article-tile.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="card"
-    @mouseover="hovered = !hovered"
-    @mouseleave="hovered = !hovered">
-    <router-link :to="url" v-on:click.native="select(id)" class="card-link">{{title}}</router-link>
-    <div class="card-image ri">
-      <figure :class="'image is-16by9'+ (hovered ? ' hovered': '')">
-        <img v-lazy="imgUrl" :alt="imgAlt">
-      </figure>
+    <div class="card"
+        @mouseover="hovered = !hovered"
+        @mouseleave="hovered = !hovered">
+      <router-link :to="url" v-on:click.native="select(id)" class="card-link">{{title}}</router-link>
+        <div class="card-image ri">
+          <figure class="image is-16by9">
+            <transition name="image-zoom">
+              <img v-once="hovered" v-lazy="imgUrl" :alt="imgAlt">
+            </transition>
+          </figure>
+        </div>
+      <div class="card-content draw meet">
+          <p class="title is-5 is-spaced">
+            <router-link :to="url">{{title}}</router-link>
+          </p>
+          <p class="subtitle is-6">
+            {{text}}
+          </p>
+      </div>
     </div>
-    <div class="card-content">
-        <p class="title is-5 is-spaced">
-          <router-link :to="url">{{title}}</router-link>
-        </p>
-        <p class="subtitle is-6">
-          {{text}}
-        </p>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -53,8 +55,11 @@ export default {
     color: $water;
 
     .card-content {
-      border: 2px solid $water;
-      background: transparent;
+      position: relative;
+      vertical-align: middle;
+      padding: 10px;
+      z-index: 2;
+      pointer-events: none;
       margin-top: 4%;
     }
   }
@@ -88,20 +93,6 @@ export default {
     }
 
     .image {
-      -webkit-transition: all 1s ease;
-      -moz-transition: all 1s ease;
-      -ms-transition: all 1s ease;
-      -o-transition: all 1s ease;
-      transition: all 1s ease;
-
-      &.hovered {
-        -webkit-transform:scale(1.25);
-        -moz-transform:scale(1.25);
-        -ms-transform:scale(1.25);
-        -o-transform:scale(1.25);
-        transform:scale(1.25);
-      }
-
       &:after {
         content: "";
         display: block;
@@ -115,12 +106,55 @@ export default {
     }
   }
 
-  .card-content {
-    position: relative;
-    background-color: #fff;
-    padding: 10px;
-    transition: border-color .5s;
-    z-index: 2;
-    pointer-events: none;
+  .image-zoom-enter,
+  .image-zoom-leave-to {
+    -webkit-transform:scale(1.25);
+    -moz-transform:scale(1.25);
+    -ms-transform:scale(1.25);
+    -o-transform:scale(1.25);
+    transform:scale(1.25);
   }
+  .image-zoom-enter-active,
+  .image-zoom-leave-active {
+    -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -ms-transition: all 1s ease;
+    -o-transition: all 1s ease;
+    transition: all 1s ease;
+  }
+
+// .center {
+//   &:hover {
+//     color: $purple;
+//   }
+
+//   // Set up base styles, we're going to scale instead of animating width/height
+//   &::before,
+//   &::after {
+//     top: 0;
+//     left: 0;
+//     height: 100%;
+//     width: 100%;
+//     transform-origin: center; // Ensure scaling is done from the center (expands outwards)
+//   }
+
+//   // scale3d(<scale-horizontal>, <scale-vertical>, <scale-depth>);
+//   &::before {
+//     border-top: 2px solid $purple;
+//     border-bottom: 2px solid $purple;
+//     transform: scale3d(0,1,1); // Shrink only width
+//   }
+
+//   &::after {
+//     border-left: 2px solid $purple;
+//     border-right: 2px solid $purple;
+//     transform: scale3d(1,0,1); // Shrink only height
+//   }
+
+//   &:hover::before,
+//   &:hover::after {
+//     transform: scale3d(1,1,1); // Show full-size
+//     transition: transform 0.5s;
+//   }
+// }
 </style>
