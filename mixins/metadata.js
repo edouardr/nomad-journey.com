85a45f421768b7d02d60142e9558eebcbbb8f531
edouardr <riviere.edouard@gmail.com>
2018-06-19 19:45:47 +0100

    Use ogImage description for og:image:alt property

--- mixins/metadata.js
+++ mixins/metadata.js
@@ -7,7 +7,7 @@ export default {
           { hid: 'keywords', name: 'keywords', content: page.metaKeywords.value },
           { hid: 'og:title', property: 'og:title', content: page.ogTitle.value },
           { hid: 'og:image', property: 'og:image', content: page.ogImage ? page.ogImage.value[0].url : '' },
-          { hid: 'og:image:alt', property: 'og:image:alt', content: page.ogImage ? page.ogImage.value[0].text : '' },
+          { hid: 'og:image:alt', property: 'og:image:alt', content: page.ogImage ? page.ogImage.value[0].description : '' },
           { hid: 'og:description', property: 'og:description', content: page.ogDescription.value }
         ],
         title: `${page.metaTitle.value} | Nomad-Journey`
