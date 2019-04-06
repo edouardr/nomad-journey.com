3d816cd56c49f998a0a4ac88b4da0cc60cdb2ef3
edouardr <riviere.edouard@gmail.com>
2019-04-05 21:32:32 -0500

    Header styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,60 +1,80 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { Link, graphql, StaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
+import './header.css';
 
-const Header = ({siteTitle}) => (
-  <StaticQuery
-    query={graphql`
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <StaticQuery
+      query={graphql`
     query {
       file(relativePath: { eq: "logo-transp.png" }) {
         childImageSharp {
-          fluid(maxWidth: 700) {
-            ...GatsbyImageSharpFluid_noBase64
+          fixed(width: 125) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      allKenticoCloudItemNavigationItem {
+        edges {
+          node {
+            system {
+              language
+            }
+            elements {
+              title {
+                value
+              }
+              redirect_to_url {
+                value
+              }
+              slug {
+                value
+              }
+              appearance__sort_order {
+                value
+              }
+            }
           }
         }
       }
     }
   `}
-  render={ data => (
-    <div
-      style={{
-        background: `rebeccapurple`,
-        marginBottom: `1.45rem`,
-      }}
-    >
-      <Link
-        className="navbar-item brand"
-        to="/"
-      >
-        <Img fixed={data.file.childImageSharp.fluid} />
-      </Link>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `1.45rem 1.0875rem`,
-        }}
-      >
-        <h1 style={{margin: 0}}>
-          <Link
-            to="/"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-      </div>
-    </div>
-  )} />
-);
+      render={data => {
+        const menuLinks = data.allKenticoCloudItemNavigationItem.edges
+          .filter(edge => edge.node.system.language === 'en')
+          .map(edge => edge.node)
+          .sort((src, cmp) => parseInt(src.elements.appearance__sort_order.value) - parseInt(cmp.elements.appearance__sort_order.value));
 
-Header.propTypes = {
-  data: PropTypes.object,
-  siteTitle: PropTypes.string,
+        return (
+          <header className="navbar is-transparent">
+            <div className="container">
+              <div className="navbar-brand">
+                <Link
+                  className="navbar-item brand"
+                  to="/"
+                >
+                  <Img fixed={data.file.childImageSharp.fixed} />
+                </Link>
+                <span className={`navbar-burger burger ${(isOpen ? 'is-active' : '')}`} data-target="navMenu" onClick={() => setIsOpen(!isOpen)}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              <div id="navMenu" className={`navbar-menu ${(isOpen ? 'is-active' : '')}`}>
+                <div className="navbar-end">
+                  {
+                    menuLinks.map(link => <Link key={link.elements.slug.value} className="navbar-item" to={link.elements.redirect_to_url.value}>{link.elements.title.value}</Link>)
+                  }
+                </div>
+              </div>
+            </div>
+          </header>
+        );
+      }} />
+  );
 };
 
 export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,19 +4,11 @@ import Header from '../components/header';
 import SEO from './SEO';
 
 const Layout = ({children, item}) => {
-  const title = item ? item.elements.page_metadata__og_title.value : 'No title';
   return (
     <div>
       <SEO item={item} />
-      <Header siteTitle={title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <Header />
+      <div>
         {children}
       </div>
     </div>
