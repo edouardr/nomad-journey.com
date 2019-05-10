9447cae984a3e993da10e1a97934fa1bbebdfa03
edouardr <riviere.edouard@gmail.com>
2019-05-10 16:25:08 -0500

    Styling

--- src/components/Newsletter/mailChimpSubscription.js
+++ src/components/Newsletter/mailChimpSubscription.js
@@ -8,12 +8,12 @@ const MailChimpSubscription = () => {
   const [success, setSuccess] = React.useState(false);
   const [submitted, setSubmitted] = React.useState(false);
   const [listFields, setListFields] = React.useState({
-    FNAME:''
+    FNAME: ''
   });
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if(!email) {
+    if (!email) {
       return;
     }
     const { result, msg } = await addToMailchimp(email, listFields);
@@ -23,37 +23,39 @@ const MailChimpSubscription = () => {
   return (
     <div className={`${styles.formContainer}`}>
       <section className="section">
-        <div className="columns is-vcentered">
-          <div className="cloumn has-text-centered">
-            <p className="title has-text-white is-size-4">Get notified when we release a new article!</p>
-          </div>
-          <div className="column">
-            <form onSubmit={handleSubmit}>
-              <div className="field is-horizontal">
-                <div className="field-body">
-                  <div className="field">
-                    <p className="control has-icons-left has-icons-right">
-                      <input className="input is-medium is-flat" type="text" placeholder="First Name" onChange={event => setListFields({FNAME:event.target.value})}/>
-                      <span className="icon is-small is-left">
-                        <FontAwesomeIcon icon="user" />
-                      </span>
-                    </p>
-                  </div>
+        <div className={`container ${styles.formContainer}`}>
+          <div className="columns is-vcentered">
+            <div className="cloumn has-text-centered">
+              <p className="title has-text-white is-size-4">Get notified when we release a new article!</p>
+            </div>
+            <div className="column">
+              <form onSubmit={handleSubmit}>
+                <div className="field is-horizontal">
+                  <div className="field-body">
+                    <div className="field">
+                      <p className="control has-icons-left has-icons-right">
+                        <input className="input is-medium is-flat" type="text" placeholder="First Name" onChange={event => setListFields({ FNAME: event.target.value })} />
+                        <span className="icon is-small is-left">
+                          <FontAwesomeIcon icon="user" />
+                        </span>
+                      </p>
+                    </div>
 
-                  <div className="field">
-                    <p className="control has-icons-left has-icons-right">
-                      <input className="input is-medium is-flat" type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} />
-                      <span className="icon is-small is-left">
-                        <FontAwesomeIcon icon="envelope" />
-                      </span>
-                    </p>
-                  </div>
-                  <div className="control">
-                    <button type="submit" className="button is-primary is-medium">Subscribe</button>
+                    <div className="field">
+                      <p className="control has-icons-left has-icons-right">
+                        <input className="input is-medium is-flat" type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} />
+                        <span className="icon is-small is-left">
+                          <FontAwesomeIcon icon="envelope" />
+                        </span>
+                      </p>
+                    </div>
+                    <div className="control">
+                      <button type="submit" className="button is-primary is-medium">Subscribe</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </section>
