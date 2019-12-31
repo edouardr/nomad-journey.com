b274758bcd0613db007f054d008579318831f205
edouardr <riviere.edouard@gmail.com>
2019-12-31 11:02:52 +0100

    Update linting settings

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    `eslint:recommended`,
+    `eslint-config-prettier`,
+    `plugin:react/recommended`,
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  rules: {
+    strict: ['error', 'never'],
+  },
+};
