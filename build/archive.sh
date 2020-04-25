f3db10ffb071f58aab6643d069cc86e7b9ef39b7
edouardr <riviere.edouard@hotmail.fr>
2020-04-25 17:40:23 +0200

    Archive with latest build version

--- build/archive.sh
+++ build/archive.sh
@@ -2,7 +2,28 @@
 set -euo pipefail
 
 now="$(date --utc --iso-8601=seconds)"
+lastTag="$(git describe --abbrev=0 --tags)"
 
+#!/usr/bin/env bash
+IS_PROD=0
+TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$now"
+usage() {             
+  echo "Usage: $0 [ -p ]" 1>&2 
+}
+exit_abnormal() {     
+  usage
+  exit 1
+}
+while getopts ":p:" options; do
+  case "${options}" in
+    p)                
+      IS_PROD=1  
+      ;;
+  esac
+done
+if [ IS_PROD = 1 ]; then
+  TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$lastTag"
+fi
 echo "archiving"
-aws s3 mv "s3://$BUCKET/$UPLOAD_DIR" "s3://$BUCKET/archive/$UPLOAD_DIR/$now" \
+aws s3 mv "s3://$BUCKET/$UPLOAD_DIR" "$TARGET" \
   --recursive
