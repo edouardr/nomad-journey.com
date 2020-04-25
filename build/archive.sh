fee02b9dff9994ed086803892f2e4cb049877e35
edouardr <riviere.edouard@hotmail.fr>
2020-04-25 19:07:45 +0200

    Unbound variable

--- build/archive.sh
+++ build/archive.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 LATEST_TAG="WIP" #$(git describe --abbrev=0 --tags)"
-NOW="$(date --utc --iso-8601=seconds)"
+now="$(date --utc --iso-8601=seconds)"
 IS_PROD=0
-TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$NOW"
+TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$now"
 
 usage() {
   echo "Usage: $0 [ -p ]" 1>&2 
@@ -23,5 +23,4 @@ if [ "$IS_PROD" = "1" ]; then
   TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$LATEST_TAG"
 fi
 echo "archiving"
-aws s3 mv "s3://$BUCKET/$UPLOAD_DIR" "s3://$BUCKET/archive/$UPLOAD_DIR/$now" \
-  --recursive
+aws s3 mv "s3://$BUCKET/$UPLOAD_DIR" "$TARGET" --recursive
