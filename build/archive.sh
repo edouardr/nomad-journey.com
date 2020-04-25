4acbdb8abb57fc7df8b9515855f90a86d8c8436d
edouardr <riviere.edouard@hotmail.fr>
2020-04-25 18:15:10 +0200

    Fix archiving script - use bash getopts correctly

--- build/archive.sh
+++ build/archive.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-now="$(date --utc --iso-8601=seconds)"
-lastTag="$(git describe --abbrev=0 --tags)"
-
-#!/usr/bin/env bash
+LATEST_TAG="$(git describe --abbrev=0 --tags)"
+NOW="$(date --utc --iso-8601=seconds)"
 IS_PROD=0
-TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$now"
-usage() {             
+TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$NOW"
+
+usage() {
   echo "Usage: $0 [ -p ]" 1>&2 
 }
-exit_abnormal() {     
-  usage
-  exit 1
-}
-while getopts ":p:" options; do
+while getopts "ph" options; do
   case "${options}" in
-    p)                
+    p)
       IS_PROD=1  
       ;;
+    h)
+      usage
+      exit 0
   esac
 done
-if [ IS_PROD = 1 ]; then
-  TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$lastTag"
+if [ "$IS_PROD" = "1" ]; then
+  TARGET="s3://$BUCKET/archive/$UPLOAD_DIR/$LATEST_TAG"
 fi
 echo "archiving"
-aws s3 mv "s3://$BUCKET/$UPLOAD_DIR" "$TARGET" \
+aws s3 mv "s3://$BUCKET/$UPLOAD_DIR" "s3://$BUCKET/archive/$UPLOAD_DIR/$now" \
   --recursive
